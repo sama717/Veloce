37292e4977ed27d04e5d4056038a9b74ca6ecd70
sama717 <samafarag7@gmail.com>
2026-06-10 20:37:00 +0300

    feat: set up jwt authentication

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Veloce.Mapping;
 using Veloce.Repository;
 using Veloco.Data;
@@ -13,6 +16,9 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
         var licenseKey = builder.Configuration["Automapper:LicenseKey"];
         var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        var jwtKey =  builder.Configuration["Jwt:Key"];
+        var jwtIssuer = builder.Configuration["Jwt:Issuer"];
+        var jwtAudience = builder.Configuration["Jwt:Audience"];
 
         // Add services to the container.
 
@@ -37,6 +43,25 @@ public class Program
         builder.Services.AddScoped<IAssetOwnershipRepository, AssetOwnershipRepository>();
         builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtIssuer,
+                ValidAudience = jwtAudience,
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(jwtKey))
+            };
+        });
+        
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
@@ -46,10 +71,11 @@ public class Program
         }
 
         app.UseHttpsRedirection();
+        
+        app.UseAuthentication();
 
         app.UseAuthorization();
-
-
+        
         app.MapControllers();
 
         app.Run();
---
+++ Services/TokenService.cs
@@ -0,0 +1,37 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Veloco.Interfaces;
+using Veloco.Models;
+
+namespace Veloce.Services;
+
+public class TokenService(IConfiguration configuration) : ITokenService
+{
+    private readonly IConfiguration _configuration = configuration;
+
+    public string GenerateToken(User user)
+    {
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Ke"]!));
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Email, user.Email),
+            new Claim(ClaimTypes.Role, user.Role.ToString()),
+            new Claim(ClaimTypes.Name, user.Username)
+        };
+
+        var token = new JwtSecurityToken(
+            issuer: _configuration["Jwt:Issuer"],
+            audience: _configuration["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(
+                int.Parse(_configuration["Jwt:ExpiryMinutes"]!)),
+            signingCredentials: credentials
+        );
+        
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
